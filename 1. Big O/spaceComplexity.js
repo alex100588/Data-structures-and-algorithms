86930ffc4e0f76c5581cd3a26cf4a846c2353d30
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-09-28 16:50:05 +0300

    Add files via upload

---
+++ 1. Big O/spaceComplexity.js
@@ -0,0 +1,21 @@
+function booooo(n){
+    for(let i=0; i<n.length; i++){
+        console.log('Booo');
+    }
+}
+
+booooo([1,2,3,4,5]);
+
+
+function arrayOfNTymes(n){
+    let hiArray = []
+    for (let i = 0; i < n; i++) {
+        hiArray[i] = 'hi'
+    }
+    return hiArray
+}
+
+console.log(arrayOfNTymes(6));
+
+
+

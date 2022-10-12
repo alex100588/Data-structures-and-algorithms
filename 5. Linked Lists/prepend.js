8e16f960afc9b4bb72ec2e98be52183d779aa515
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-10-12 15:42:07 +0300

    Add files via upload

---
+++ 5. Linked Lists/prepend.js
@@ -0,0 +1,40 @@
+class LinkedList{
+    constructor(value){
+        this.head ={
+            value: value,
+            next: null
+        }
+        this.tail = this.head
+        this.length = 1
+    }
+
+   append(value){
+        const newNode = {
+            value: value,
+            next: null
+        }
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+        return this
+    }
+
+    prepend(value){
+        const newNode = {
+            value: value,
+            next: null
+        }
+
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
+    }
+}
+
+const myLinkedList = new LinkedList(10)
+myLinkedList.prepend(5)
+myLinkedList.prepend(15)
+myLinkedList.prepend(25)
+
+console.log(myLinkedList);

f51842a913aedab54ad4469d678d4ba2c91c997c
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-10-25 16:34:19 +0300

    Add files via upload

---
+++ 5. Linked Lists/insert.js
@@ -0,0 +1,79 @@
+class LinkedList{
+    constructor(value){
+        this.head ={
+            value: value,
+            next: null
+        }
+        this.tail = this.head
+        this.length = 1
+    }
+  
+   append(value){
+        const newNode = {
+            value: value,
+            next: null
+        }
+        this.tail.next = newNode
+        this.tail = newNode
+        this.length++
+        return this
+    }
+  
+    prepend(value){
+        const newNode = {
+            value: value,
+            next: null
+        }
+  
+        newNode.next = this.head
+        this.head = newNode
+        this.length++
+        return this
+    }
+  
+    printList(){
+      const array = []
+      let currentNode = this.head
+      while(currentNode !== null){
+        array.push(currentNode.value)
+        currentNode = currentNode.next
+      }
+      return array
+    }
+  
+    insert(index, value){
+      if(index >= this.length) {
+        console.log('yes')
+        return this.append(value);
+      }
+      
+      const newNode = {
+        value: value,
+        next: null
+      }
+      const leader = this.traverseToIndex(index-1);
+      const holdingPointer = leader.next;
+      leader.next = newNode;
+      newNode.next = holdingPointer;
+      this.length++;
+      return this.printList();
+    }
+    traverseToIndex(index) {
+      let counter = 0;
+      let currentNode = this.head;
+      while(counter !== index){
+        currentNode = currentNode.next;
+        counter++;
+      }
+      return currentNode;
+    }
+  }
+  
+  const myLinkedList = new LinkedList(10)
+  myLinkedList.prepend(5)
+  myLinkedList.prepend(15)
+  myLinkedList.prepend(25)
+  myLinkedList.insert(2,100)
+  myLinkedList.insert(1,50)
+  
+  console.log(myLinkedList.printList());

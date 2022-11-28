4c08651dea8c882684241988897e43a01f3d6574
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-11-28 18:22:04 +0200

    Add files via upload

---
+++ 8. Graphs/graph.js
@@ -0,0 +1,16 @@
+//      2 - 0
+//    1 - - - 3
+
+// Edge list
+const graph = [[0, 2], [2, 3], [2, 1], [1, 3]]
+
+// Adjacent list
+const graph2 = [[2], [2, 3], [0, 1, 3], [1, 2]]
+
+// Adjacent matrix
+const graph3 = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [1, 1, 1, 0],
+    [0, 1, 1, 0]
+]

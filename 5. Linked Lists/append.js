4e80d741c558020006850575131ac2a45ba3e31a
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-12-15 11:38:17 +0200

    Update append.js

--- 5. Linked Lists/append.js
+++ 5. Linked Lists/append.js
@@ -1,27 +1,34 @@
-class LinkedList{
-    constructor(value){
-        this.head ={
-            value: value,
-            next: null
-        }
-        this.tail = this.head
-        this.length = 1
-    }
-
-   append(value){
-        const newNode = {
-            value: value,
-            next: null
-        }
-        this.tail.next = newNode
-        this.tail = newNode
-        this.length++
-        return this
-    }
+class Node{
+  constructor(value){
+    this.value = value
+    this.nex = null
+  }
 }
 
-const myLinkedList = new LinkedList(10)
-myLinkedList.append(5)
-myLinkedList.append(16)
-myLinkedList.append(22)
+class SinglyLinkedList{
+  constructor(value){
+    this.head = new Node(value)
+    this.tail = this.head
+    this.next = null
+    this.length++
+  }
+
+  append(value){
+    const newNode = new Node(value)
+    if(!this.head){
+      this.head = newNode
+    }else{
+      this.tail.next = newNode
+      this.tail = newNode
+      this.length++
+      return this
+    }
+  }
+
+}
+
+let myLinkedList = new SinglyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(15);
+myLinkedList.append(16);
 console.log(myLinkedList);

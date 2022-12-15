9b045aa9de2aff9bfb5070717be8e86865be1c6a
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-12-15 11:38:40 +0200

    Update prepend.js

--- 5. Linked Lists/prepend.js
+++ 5. Linked Lists/prepend.js
@@ -1,40 +1,47 @@
-class LinkedList{
-    constructor(value){
-        this.head ={
-            value: value,
-            next: null
-        }
-        this.tail = this.head
-        this.length = 1
-    }
-
-   append(value){
-        const newNode = {
-            value: value,
-            next: null
-        }
-        this.tail.next = newNode
-        this.tail = newNode
-        this.length++
-        return this
-    }
-
-    prepend(value){
-        const newNode = {
-            value: value,
-            next: null
-        }
-
-        newNode.next = this.head
-        this.head = newNode
-        this.length++
-        return this
-    }
+class Node{
+  constructor(value){
+    this.value = value
+    this.nex = null
+  }
 }
 
-const myLinkedList = new LinkedList(10)
-myLinkedList.prepend(5)
-myLinkedList.prepend(15)
-myLinkedList.prepend(25)
+class SinglyLinkedList{
+  constructor(value){
+    this.head = new Node(value)
+    this.tail = this.head
+    this.next = null
+    this.length++
+  }
 
+  append(value){
+    const newNode = new Node(value)
+    if(!this.head){
+      this.head = newNode
+    }else{
+      this.tail.next = newNode
+      this.tail = newNode
+      this.length++
+      return this
+    }
+  }
+
+  prepend(value){
+    const newNode = new Node(value)
+    if(!this.head){
+      this.head = newNode
+    }else{
+      newNode.next = this.head
+      this.head = newNode
+      this.length++
+      return this
+    }
+  }
+}
+
+let myLinkedList = new SinglyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(15);
+myLinkedList.append(16);
+myLinkedList.prepend(1)
+myLinkedList.prepend(2)
 console.log(myLinkedList);

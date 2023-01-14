db7942e4d099172d43a4c7a65a31210e30c7d337
alex100588 <49831245+alex100588@users.noreply.github.com>
2023-01-14 18:54:48 +0200

    Update insertionSort.js

--- 10. Sorting algorithms/insertionSort.js
+++ 10. Sorting algorithms/insertionSort.js
@@ -1,21 +1,16 @@
-const numbers = [1,5,2,4,3];
-const numbers2 = [11,22,33,543,0,12];
-
-
-function insertionSort(array){
-    for (let i = 0; i < array.length; i++) {
-        for (let j = i; j > 0; j--) {
-            if(array[j] < array[j-1]){
-                // let temp = array[j]
-                // array[j] = array[j-1]
-                // array[j-1] = temp
-                [array[j], array[j-1]] = [array[j-1], array[j]]
-            }else{
-                break
-            }
-        }
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = array.length; j>0;j--) {
+      if (array[j] < array[j -1]) {
+        let swap = array[j]
+        array[j] = array[j-1]
+        array[j-1] = swap
+        
+      }
     }
-    return array
+  }
+  return array
 }
-console.log(insertionSort(numbers))
-console.log(insertionSort(numbers2))
+
+console.log(insertionSort([5,2,3,6,7,4]));
+console.log(insertionSort([22,12, 55,1]));
